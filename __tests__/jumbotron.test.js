a3efa4eb66c839b020c2a166c3b5b4f9497cbf25
Pavel Sirotkin <pav.pnz@gmail.com>
2020-11-02 00:02:52 +0100

    tests

---
+++ __tests__/jumbotron.test.js
@@ -0,0 +1,13 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import app from '../src/jumbotron';
+
+beforeEach(async () => {
+  const initHtml = await fs.readFile(path.join(__dirname, '__fixtures__', 'index.html'), 'utf-8');
+  document.body.innerHTML = initHtml;
+});
+
+test('init', () => {
+  app();
+  expect(true).toBeDefined();
+});
