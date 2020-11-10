2de6c79810f8eea0fe96b227c92784726da2678c
Pavel Sirotkin <pav.pnz@gmail.com>
2020-11-10 18:33:29 +0100

    fix tests

--- __tests__/jumbotron.test.js
+++ __tests__/jumbotron.test.js
@@ -1,13 +1,22 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import $ from 'jquery';
 import app from '../src/jumbotron';
 
 beforeEach(async () => {
   const initHtml = await fs.readFile(path.join(__dirname, '__fixtures__', 'index.html'), 'utf-8');
-  document.body.innerHTML = initHtml;
+  document.write(initHtml);
+  app();
 });
 
-test('init', () => {
-  app();
+test('init', async () => {
+  expect(document.body.outerHTML).toMatchSnapshot();
+});
+
+test('invalid rss', async () => {
+  const input = document.querySelector('.form-control');
+  $(input).val('lalalal').trigger('change');
+  const form = document.querySelector('.rss-form');
+  $(form).on('submit');
   expect(document.body.outerHTML).toMatchSnapshot();
 });
