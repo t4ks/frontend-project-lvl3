27b2c59eb93e67c2269769c696f36bd09aaa47d1
Pavel Sirotkin <pav.pnz@gmail.com>
2020-12-10 23:07:38 +0100

    change you validation to notOneOf

--- src/form-validation.js
+++ src/form-validation.js
@@ -1,14 +1,11 @@
 import * as yup from 'yup';
-import _ from 'lodash';
 
 const getFormValidationSchema = (state) => (
   yup.object().shape({
     rssUrl: yup
       .string()
       .url('Please input a valid URL')
-      .test('check-rss-url-uniq', 'Rss already exist', (value) => (
-        _.find(state.feeds, (f) => f.rssUrl === value) === undefined
-      )),
+      .notOneOf(state.feeds.map((f) => f.rssUrl), 'Rss already exist'),
   })
 );
 
